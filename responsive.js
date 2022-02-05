0067a5e6ba9b6740c8e984f92a3f9d52e9a4427f
VarunCypherV <varunvk2004@gmail.com>
2022-02-05 11:36:47 +0530

    my project

---
+++ responsive.js
@@ -0,0 +1,11 @@
+(function($) {
+    $(document).ready(function() {
+        $('.slider').slick({
+            dots: true,
+            infinite: true,
+            cssEase:'horizontal',
+            swipe: false,
+        });
+    });
+
+})( jQuery );
